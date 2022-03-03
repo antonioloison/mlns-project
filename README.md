a07eeedd718b18e6452825216944635691a369a8
António Loison <antonio.loison@student-cs.fr>
2022-03-03 19:43:56 +0100

    Add dataset loader and requirements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # mlns-project
 Deep Learning based Graph embedding for nodes clustering
+Project for the Machine Learning in Network Science class at CentraleSupélec.
+ 
---
+++ dataset.py
@@ -0,0 +1,16 @@
+from dgl.data import CoraGraphDataset, CiteseerGraphDataset, PubmedGraphDataset
+
+
+def get_dataset(dataset_name: str):
+    if dataset_name == "cora":
+        return CoraGraphDataset()
+    elif dataset_name == "cite":
+        return CiteseerGraphDataset()
+    elif dataset_name == "pubmed":
+        return PubmedGraphDataset()
+    else:
+        raise ValueError(f"No dataset corresponds to {dataset_name}")
+
+
+if __name__ == "__main__":
+    print(get_dataset("cora"))
